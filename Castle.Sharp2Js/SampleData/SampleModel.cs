cc9c4aed964c9eb1046cc320d0d056abd3e4fcad
Grant Hamm <tghamm@gmail.com>
2015-10-22 00:36:14 -0400

    Includes NuGet package deployment, and updated sample

---
+++ Castle.Sharp2Js/SampleData/SampleModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Castle.Sharp2Js.SampleData
+{
+    public class AddressInformation
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+        public OwnerInformation Owner { get; set; }
+        public List<Feature> Features { get; set; }
+        public List<string> Tags { get; set; }
+    }
+
+    public class OwnerInformation
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Feature
+    {
+        public string Name { get; set; }
+        public double Value { get; set; }
+    }
+}
